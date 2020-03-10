205186d34e6dc67af54a5a773ff05919d79f5e54
Eric.Cheng <eric.cheng@sifive.com>
2020-03-09 19:32:26 -0700

    first commit

---
+++ test_0309.sh
@@ -0,0 +1,4 @@
+rm -rf /scratch/ericc/work/make_fed_test/builds/coreip_e31_ahb_try
+rm -rf /scratch/ericc/work/make_fed_test/rocket-chip/lib
+python3 replaceSim_0309.py /home/ericc/work/wake_2020_02_11/workspace/build/product-coreip-sifive/e31 /home/ericc/work/make_fed_test
+
